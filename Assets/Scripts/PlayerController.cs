0659f54f72b6ca69030caafe90cbedbd2d46dc58
proGRAmist24578 <108571896+proGRAmist24578@users.noreply.github.com>
2025-04-23 19:29:31 +0300

    add player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 movementInput;
+    private Rigidbody2D rb;
+
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction interactAction;
+    private InputAction exitAction;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        moveAction = playerInput.actions["Move"];
+        interactAction = playerInput.actions["Interact"];
+        exitAction = playerInput.actions["Exit"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        interactAction.performed += OnInteract;
+        exitAction.performed += OnExit;
+    }
+
+    private void OnDisable()
+    {
+        interactAction.performed -= OnInteract;
+        exitAction.performed -= OnExit;
+    }
+
+    private void Update()
+    {
+        movementInput = moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        // Тут можно делать Raycast или проверку триггера
+        Debug.Log("Interact pressed!");
+        // Пример: открыть UI с информацией об артефакте
+    }
+
+    private void OnExit(InputAction.CallbackContext context)
+    {
+        Debug.Log("Exit pressed!");
+        // Пример: закрыть UI с информацией
     }
 }
